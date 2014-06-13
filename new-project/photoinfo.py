c7bcb9f21619902e013d11af54a2b4c678cb9f14
Thomas Müller <tho.mueller1@gmail.com>
2014-06-14 00:50:20 +0200

    photoinfo: more general version of caching

--- new-project/photoinfo.py
+++ new-project/photoinfo.py
@@ -11,10 +11,10 @@ class MemoryCache(object):
 	def __init__(self):
 		self.cache = {}
 	
-	def load_cached(self, input_file, *exiftool_arguments):
-		key = tuple([input_file]+list(exiftool_arguments))
+	def load_cached(self, input_file, *args):
+		key = tuple([input_file]+list(args))
 		if key not in self.cache:
-			self.cache[key] = self.function(input_file, *exiftool_arguments)
+			self.cache[key] = self.function(input_file, *args)
 		return self.cache[key]
 	
 	def __call__(self, function):
@@ -24,14 +24,14 @@ class MemoryCache(object):
 
 class FileCache(object):
 	
-	def load_cached(self, input_file, *exiftool_arguments):
-		cache_file = input_file+"."+hashlib.md5(str(exiftool_arguments)).hexdigest()
+	def load_cached(self, input_file, *args):
+		cache_file = input_file+"."+hashlib.md5(str(args)).hexdigest()
 		cache_file = os.path.join(os.path.dirname(cache_file), "."+os.path.basename(cache_file))
 		if os.path.exists(cache_file):
 			with open(cache_file, "r") as cache:
 				return cache.read()
 		else:
-			exif_info = self.function(input_file, *exiftool_arguments)
+			exif_info = self.function(input_file, *args)
 			with open(cache_file, "w") as cache:
 				cache.write(exif_info)
 			return exif_info
@@ -45,9 +45,4 @@ class FileCache(object):
 @FileCache()
 def load_exif_field(input_file, *exiftool_arguments):
 	return subprocess.check_output(["exiftool"]+list(exiftool_arguments)+["-s3", input_file]).replace("\n", "")
-
-
-for index in xrange(10):
-	for arg in sys.argv[1:]:
-		print arg, load_exif_field(arg, "-d", "%Y:%m:%d %H:%M:%S", "-DateTimeOriginal")
 
