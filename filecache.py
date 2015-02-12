5671bfc6ff43282752443a6a04a1cf55b5ba8b1f
Thomas Müller <tho.mueller1@gmail.com>
2015-02-12 22:08:33 +0100

    filecache.py: improved code, added FileProducerCache decorator

--- filecache.py
+++ filecache.py
@@ -1,40 +1,74 @@
 
 # -*- coding: utf-8 -*-
 
+import abc
 import hashlib
 import os
+import shutil
+import tempfile
 
 
-class MemoryCache(object):
+class Cache(object):
+	def __call__(self, function_to_cache):
+		self._function_to_cache = function_to_cache
+		return self._get_cached
+	
+	@abc.abstractmethod
+	def _get_cached(self, *args, **kwargs):
+		pass
+
+
+class MemoryCache(Cache):
 	def __init__(self):
 		self.cache = {}
 	
-	def load_cached(self, input_file, *args):
-		key = tuple([input_file]+list(args))
-		if key not in self.cache:
-			self.cache[key] = self.function(input_file, *args)
-		return self.cache[key]
-	
-	def __call__(self, function):
-		self.function = function
-		return self.load_cached
+	def _get_cached(self, *args, **kwargs):
+		if args not in self.cache:
+			self.cache[args] = self._function_to_cache(*args, **kwargs)
+		return self.cache[args]
 
 
-class FileCache(object):
+class FileCache(Cache):
+	def _determine_cache_file(self, *args, **kwargs):
+		args_hash = hashlib.md5("".join([str(a) for a in args])).hexdigest()
+		
+		for arg in list(args) + kwargs.keys():
+			if isinstance(arg, basestring) and os.path.exists(arg):
+				directory = os.path.dirname(arg)
+				filename, extension = os.path.splitext(os.path.basename(arg))
+				extension = kwargs.get("output_file_extension", extension)
+				cache_dir = os.path.join(directory, "cache")
+				if not os.path.exists(cache_dir):
+					os.makedirs(cache_dir)
+				return os.path.join(cache_dir, filename + "." + args_hash + extension)
+		
+		# no input file found, create cache file in temp dir
+		cache_dir = os.path.join(tempfile.gettempdir(), "cache")
+		if not os.path.exists(cache_dir):
+			os.makedirs(cache_dir)
+		return os.path.join(cache_dir, args_hash)
 	
-	def load_cached(self, input_file, *args):
-		cache_file = input_file+"."+hashlib.md5(str(args)).hexdigest()
-		cache_file = os.path.join(os.path.dirname(cache_file), "."+os.path.basename(cache_file))
+	def _get_cached(self, input_file, *args, **kwargs):
+		cache_file = self._determine_cache_file(*args, **kwargs)
 		if os.path.exists(cache_file):
 			with open(cache_file, "r") as cache:
 				return cache.read()
 		else:
-			exif_info = self.function(input_file, *args)
+			result = self._function_to_cache(*args, **kwargs)
 			with open(cache_file, "w") as cache:
-				cache.write(exif_info)
-			return exif_info
-	
-	def __call__(self, function):
-		self.function = function
-		return self.load_cached
+				cache.write(result)
+			return result
+
+
+class FileProducerCache(FileCache):
+
+	def _get_cached(self, *args, **kwargs):
+		cache_file = self._determine_cache_file(*args, **kwargs)
+		if not os.path.exists(cache_file):
+			produced_file = self._function_to_cache(*args, **kwargs)
+			if os.path.exists(produced_file):
+				shutil.move(produced_file, cache_file)
+			else:
+				return produced_file
+		return cache_file
 
